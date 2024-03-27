23f059070eaafa5cdd7382a6125d8d1884ccc33e
Shubham Pandey <pandeyshubham75@gmail.com>
2024-03-27 10:55:15 +0530

    added OU and budget

--- awsaccount.tf
+++ awsaccount.tf
@@ -15,8 +15,22 @@ resource "aws_organizations_account" "account" {
   email = var.root_email
   name = var.account_name
   tags = var.tags
+  parent_id = var.parent_id
   close_on_deletion=var.close_delete
   create_govcloud=var.gov_cloud
 }
 
-
+resource "aws_budgets_budget" "cost" {
+  name  = join("-", ["SN-CAM-Monthly-Budget", aws_organizations_account.account.id])
+  count = var.monthly_budget > 0 ? 1 : 0
+  budget_type  = "COST"
+  limit_amount = var.monthly_budget
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+  cost_filter {
+    name = "LinkedAccount"
+    values = [
+      aws_organizations_account.account.id
+    ]
+  }
+}
